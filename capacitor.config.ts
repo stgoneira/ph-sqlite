765c660413a590ad2fad92f754b7d087ba5c4c6a
hcslearning <learning@hardcybersoft.cl>
2024-09-12 23:32:51 -0300

    proyecto recien iniciado

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'sqlite.prueba',
+  appName: 'sqlite-prueba',
+  webDir: 'www'
+};
+
+export default config;
